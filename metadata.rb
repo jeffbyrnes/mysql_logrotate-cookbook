ea4cc82f012de87c321da67e2a7a474cbf148ac4
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-06-04 19:22:20 -0400

    Add license metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name             'mysql_logrotate'
 maintainer       'Roll No Rocks LLC'
 maintainer_email 'tom@rollnorocks.com'
+license          'MIT'
 description      'Installs/Configures log rotation for mysql_service (mysql cookbook > 6.0)'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.2'
